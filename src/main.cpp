dc6556b3ac2093c1b3d3a19746fc94bdee2343be
buko106 <buko1062000@yahoo.co.jp>
2017-06-09 22:52:17 +0900

    add output options

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,9 @@ using namespace boost::program_options;
 options_description set_options(){
   options_description opt("Options");
   opt.add_options()
-    ("help,h",                       "Show help")
-    ("input" , value<std::string>(),      "Input image file");
+    ("help,h"   ,                         "Show help")
+    ("input,i"  , value<std::string>(),   "Input image file")
+    ("output,o" , value<std::string>(),   "Output directory");
   return opt;
 }
 
@@ -21,37 +22,38 @@ int main( int argc, char* argv[] ){
   try{
     store(parse_command_line(argc, argv, opt), argmap);
   }catch (std::exception& ex){
-    std::cerr << "Unknown command line option: " << ex.what() << std::endl << opt << std::endl;
-    throw;
+    std::cerr << "[ERROR] " << ex.what() << std::endl << opt << std::endl;
+    exit(1);
   }
   notify(argmap);
   // show help
-  if( argmap.count("help") || !argmap.count("input") ) {
+  if( argmap.count("help") || !argmap.count("input") || !argmap.count("output") ){
     std::cerr << opt << std::endl;
     exit(1);
   }
   // processing
   std::string input = argmap["input"].as<std::string>();
   cv::Mat im = imread_as_grayscale(input);
+  if( im.empty() ){
+    std::cerr << "[ERROR] Unable to read image(\"" << input << "\")" << std::endl;
+    exit(1);
+  }
+
   skeleton sk(im,true);
   cv::Mat binary = sk.binary.clone();
   sk.thinning(ZHANGSUEN,true);
   
-  cv::namedWindow("gray", CV_WINDOW_AUTOSIZE|CV_WINDOW_FREERATIO);
-  cv::imshow("gray", im);
-  cv::waitKey(0);
-  cv::namedWindow("binary", CV_WINDOW_AUTOSIZE|CV_WINDOW_FREERATIO);
-  cv::imshow("binary", binary);
-  cv::waitKey(0);
-  cv::namedWindow("thinning", CV_WINDOW_AUTOSIZE|CV_WINDOW_FREERATIO);
-  cv::imshow("thinning", sk.binary);
-  cv::waitKey(0);
-
   double maxVal;
   cv::minMaxLoc(sk.thickness, NULL, &maxVal, NULL, NULL);
-  std::cout << maxVal << std::endl;
-  cv::namedWindow("thickness", CV_WINDOW_AUTOSIZE|CV_WINDOW_FREERATIO);
-  cv::imshow("thickness", sk.thickness * (255./maxVal));
+
+  cv::namedWindow("image", CV_WINDOW_AUTOSIZE|CV_WINDOW_FREERATIO);
+  cv::imshow("image", im);
+  cv::waitKey(0);
+  cv::imshow("image", binary);
+  cv::waitKey(0);
+  cv::imshow("image", sk.binary);
+  cv::waitKey(0);
+  cv::imshow("image", sk.thickness * (255./maxVal));
   cv::waitKey(0);
   
   return 0;
