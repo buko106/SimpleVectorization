4f80ed70b0b6de8b810e77532d2319d8c64ba561
buko106 <buko1062000@yahoo.co.jp>
2017-06-12 21:33:38 +0900

    change bezier header

--- src/bezier.cpp
+++ src/bezier.cpp
@@ -3,30 +3,32 @@
 #include<iostream>
 
 std::pair<double,std::vector<std::pair<double,double> > > 
-bezier_fittting( edge_t edge, BEZIER_DIM dim ){
+bezier_fittting( edge_t edge, double w_max, BEZIER_DIM dim ){
   if( dim == LINE ){
-    return bezier_line_fitting( edge );
+    return bezier_line_fitting( edge, w_max );
   }else if( dim == QUAD ){
-    return bezier_quadratic_fitting( edge );
+    return bezier_quadratic_fitting( edge, w_max );
   }else if( dim == CUBIC ){
-    return bezier_cubic_fitting( edge );
+    return bezier_cubic_fitting( edge, w_max );
   }
   
   std::cerr << "[ERROR] bezier_fitting : dim = " << dim << " is not supported" << std::endl;
   exit(1);
 }
 
-std::pair<double,std::vector<std::pair<double,double> > > bezier_line_fitting( edge_t ){
-  exit(1);
-}
-std::pair<double,std::vector<std::pair<double,double> > > bezier_quadratic_fitting( edge_t ){
-  exit(1);
-}
 
-std::pair<double,std::vector<std::pair<double,double> > > bezier_cubic_fitting( edge_t ){
+std::pair<double,std::vector<std::pair<double,double> > >
+bezier_line_fitting( edge_t edge, double w_max ){
   exit(1);
 }
 
+std::pair<double,std::vector<std::pair<double,double> > >
+bezier_quadratic_fitting( edge_t edge, double w_max ){
+  exit(1);
+}
 
-
+std::pair<double,std::vector<std::pair<double,double> > >
+bezier_cubic_fitting( edge_t edge, double w_max ){
+  exit(1);
+}
 
